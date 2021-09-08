57d2bfef1ec90b6d1ef6381246d53f735949885d
AlineLayze <88834380+AlineLayze@users.noreply.github.com>
2021-09-08 12:10:52 -0300

    Create Atividade 3.py

---
+++ Atividade 3.py
@@ -0,0 +1,129 @@
+
+#e)
+Alteracao_na_variavel = input('Alteracao_na_variavel_inserida?')
+print (Alteracao_na_variavel)
+
+var_logica1 = true
+var_logica2 = false
+ 
+print('var_logica1', var_logica2:', var_logica1 or var_logica2:')
+
+#f)
+print('As informações de configurações setadas pelo usuário são')
+
+num = input ('Digite numero_de_superficies')
+print(num)
+
+numero_de_superficies = input (numero_de_superficies)
+print ('O numero de superficies foi: %s' %(numero_de_superficies'))
+
+#g)
+
+#apertar a tecla correspondente à primeira letra do seu nome
+Nome_do_usuário = input('Digite seu nome')
+
+calibracao = input('Digite à primeira letra do seu nome')
+print(calibracao)
+Nome_do_usuário == [0] = true
+Nome_do_usuário != [0] = false
+
+calibracao = input('Digite à primeira letra do seu nome')
+print(calibracao)
+Nome_do_usuário == [0] = true
+Nome_do_usuário != [0] = false
+
+calibracao = input('Digite à primeira letra do seu nome')
+print(calibracao)
+Nome_do_usuário == [0] = true
+Nome_do_usuário != [0] = false
+
+calibracao = input('Digite à primeira letra do seu nome')
+print(calibracao)
+Nome_do_usuário == [0] = true
+Nome_do_usuário != [0] = false
+
+calibracao = input('Digite à primeira letra do seu nome')
+print(calibracao)
+Nome_do_usuário == [0] = true
+Nome_do_usuário != [0] = false
+
+calibracao = input('Digite à primeira letra do seu nome')
+print(calibracao)
+Nome_do_usuário == [0] = true
+Nome_do_usuário != [0] = false
+
+calibracao = input('Digite à primeira letra do seu nome')
+print(calibracao)
+Nome_do_usuário == [0] = true
+Nome_do_usuário != [0] = false
+
+calibracao = input('Digite à primeira letra do seu nome')
+print(calibracao)
+Nome_do_usuário == [0] = true
+Nome_do_usuário != [0] = false
+
+calibracao = input('Digite à primeira letra do seu nome')
+print(calibracao)
+Nome_do_usuário == [0] = true
+Nome_do_usuário != [0] = false
+
+calibracao = input('Digite à primeira letra do seu nome')
+print(calibracao)
+Nome_do_usuário == [0] = true
+Nome_do_usuário != [0] = false
+
+#apertar a tecla correspondente à última letra do seu nome
+
+calibracao = input('Digite à última letra do seu nome')
+print(calibracao)
+Nome_do_usuário == [-1] = true
+Nome_do_usuário != [-1] = false
+
+calibracao = input('Digite à última letra do seu nome')
+print(calibracao)
+Nome_do_usuário == [-1] = true
+Nome_do_usuário != [-1] = false
+
+calibracao = input('Digite à última letra do seu nome')
+print(calibracao)
+Nome_do_usuário == [-1] = true
+Nome_do_usuário != [-1] = false
+
+calibracao = input('Digite à última letra do seu nome')
+print(calibracao)
+Nome_do_usuário == [-1] = true
+Nome_do_usuário != [-1] = false
+
+calibracao = input('Digite à última letra do seu nome')
+print(calibracao)
+Nome_do_usuário == [-1] = true
+Nome_do_usuário != [-1] = false
+
+calibracao = input('Digite à última letra do seu nome')
+print(calibracao)
+Nome_do_usuário == [-1] = true
+Nome_do_usuário != [-1] = false
+
+calibracao = input('Digite à última letra do seu nome')
+print(calibracao)
+Nome_do_usuário == [-1] = true
+Nome_do_usuário != [-1] = false
+
+calibracao = input('Digite à última letra do seu nome')
+print(calibracao)
+Nome_do_usuário == [-1] = true
+Nome_do_usuário != [-1] = false
+
+calibracao = input('Digite à última letra do seu nome')
+print(calibracao)
+Nome_do_usuário == [-1] = true
+Nome_do_usuário != [-1] = false
+
+calibracao = input('Digite à última letra do seu nome')
+print(calibracao)
+Nome_do_usuário == [-1] = true
+Nome_do_usuário != [-1] = false
+
+#h)
+
+https://repositorio.ufu.br/bitstream/123456789/14904/1/d.pdf
